4a2b626f82b47fb460ffe59faf04ac02cd6bcfb1
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-04-02 19:39:06 +0200

    - add async to app - convert last file to typescript

--- address-book/app.ts
+++ address-book/app.ts
@@ -1,15 +1,20 @@
 import {Command} from './command';
 import {ContactFactory, JsonfileContactRepository} from './contact';
 import {Jsonfile} from "./jsonfile";
+import * as async from "async";
 
-let command = new Command(new JsonfileContactRepository(new Jsonfile()), new ContactFactory());
+let command = new Command(
+    new JsonfileContactRepository(new Jsonfile()),
+    new ContactFactory()
+);
 
-function handleError(err) {
+async.series([
+    (callback) => command.executeCurrentOperation(callback)
+],
+function(err) {
     if (err) {
-        console.log(err);
+        console.error(err);
     } else {
         console.log('OK! The command ran successfully!');
     }
-}
-
-command.executeCurrentOperation(handleError);
+});
