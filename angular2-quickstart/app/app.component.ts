2c5d739ed24d6b3a75fa687babbca6d108212c4a
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-04-09 13:32:16 +0200

    - skeleton app for angular 2

---
+++ angular2-quickstart/app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1><h2>New stuff in here...</h2>'
+})
+export class AppComponent {}
