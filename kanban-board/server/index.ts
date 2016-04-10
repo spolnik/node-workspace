263f58c8781a730e27dc13bc380835ad5e6d14ee
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-04-10 13:33:22 +0200

    - add simple server

---
+++ kanban-board/server/index.ts
@@ -0,0 +1,48 @@
+import * as restify from "restify";
+
+
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the Book",
+        description: "I should read the **whole** book",
+        color: '#BD8D31',
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
+        color: "#3A7E28",
+        status: "todo",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban Example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+];
+
+let server = restify.createServer();
+server.get('/cards', (req, res, next) => {
+    res.send(cardsList);
+    res.send(204);
+    next();
+});
+
+server.listen(3000, () => {
+    console.log(`${server.name} listening at ${server.url}`);
+});
