df4602e1e8c4e6e80181db44d4134e90cf4eb2e9
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-04-11 18:35:40 +0200

    - persist database state

--- kanban-board/server/server.ts
+++ kanban-board/server/server.ts
@@ -2,52 +2,7 @@ import * as restify from "restify";
 import {Card} from "./Card";
 import NeDBDataStore = require("nedb");
 
-let db = new NeDBDataStore();
-
-let cardsList: Card[] = [
-    {
-        id: 1,
-        title: "Read the Book",
-        description: "I should read the **whole** book",
-        color: '#BD8D31',
-        status: "in-progress",
-        tasks: []
-    },
-    {
-        id: 2,
-        title: "Write some code",
-        description: "Code along with the samples in the book. The complete source can be found at [github](https://github.com/pro-react)",
-        color: "#3A7E28",
-        status: "todo",
-        tasks: [
-            {
-                id: 1,
-                name: "ContactList Example",
-                done: true
-            },
-            {
-                id: 2,
-                name: "Kanban Example",
-                done: false
-            },
-            {
-                id: 3,
-                name: "My own experiments",
-                done: false
-            }
-        ]
-    },
-    {
-        id: 919,
-        title: "Add some refs",
-        description: "Add some references about react tutorials to awesome list",
-        color: "#e0fd00",
-        status: "done",
-        tasks: []
-    }
-];
-
-db.insert(cardsList);
+let db = new NeDBDataStore({ filename: 'kanbanDb.json', autoload: true});
 
 let server = restify.createServer();
 server.use(restify.CORS());
