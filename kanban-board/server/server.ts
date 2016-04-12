3a676a2de31ed9cb3c02021aadcb5b4e91221baa
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-04-12 17:13:53 +0200

    - cleanup server code

--- kanban-board/server/server.ts
+++ kanban-board/server/server.ts
@@ -1,9 +1,8 @@
 import * as restify from "restify";
 import {Card} from "./Card";
 import NeDBDataStore = require("nedb");
-import {CardModel} from "../app/KanbanBoard";
 
-let db = new NeDBDataStore({ filename: 'kanbanDb.json', autoload: true});
+let db = new NeDBDataStore({filename: 'kanbanDb.json', autoload: true});
 
 let server = restify.createServer();
 server.use(restify.CORS());
@@ -12,13 +11,9 @@ server.use(restify.bodyParser());
 server.get('/cards', (req, res, next) => {
 
     db.find<Card>({}, (err, docs) => {
-        if (err) {
-            next(err);
-        } else {
-            res.send(docs);
-            res.send(200);
-            next();
-        }
+        next.ifError(err);
+        res.send(200, docs);
+        next();
     });
 });
 
@@ -27,20 +22,13 @@ server.post('/cards/:cardId/tasks', (req, res, next) => {
     let newTask = JSON.parse(req.body);
 
     db.findOne<Card>({id: cardId}, (err, doc) => {
-        if (err) {
-            next(err);
-        } else {
-            doc.tasks.push(newTask);
-            db.update({id: cardId}, doc, {}, (err, numReplaced) => {
-                if (err) {
-                    next(err);
-                } else {
-                    res.send(newTask);
-                    res.send(200);
-                    next();
-                }
-            });
-        }
+        next.ifError(err);
+        doc.tasks.push(newTask);
+        db.update({id: cardId}, doc, {}, (err) => {
+            next.ifError(err);
+            res.send(200, newTask);
+            next();
+        });
     });
 });
 
@@ -49,20 +37,15 @@ server.del('/cards/:cardId/tasks/:taskId', (req, res, next) => {
     let taskId: number = Number(req.params.taskId);
 
     db.findOne<Card>({id: cardId}, (err, doc) => {
-        if (err) {
-            next(err);
-        } else {
-            let taskIndex = doc.tasks.findIndex((task) => task.id === taskId);
-            doc.tasks.splice(taskIndex, 1);
-            db.update({id: cardId}, doc, {}, (err, numReplaced) => {
-                if (err) {
-                    next(err);
-                } else {
-                    res.send(200);
-                    next();
-                }
-            });
-        }
+        next.ifError(err);
+
+        let taskIndex = doc.tasks.findIndex((task) => task.id === taskId);
+        doc.tasks.splice(taskIndex, 1);
+        db.update({id: cardId}, doc, {}, (err, numReplaced) => {
+            next.ifError(err);
+            res.send(200, numReplaced);
+            next();
+        });
     });
 });
 
@@ -71,54 +54,39 @@ server.put('/cards/:cardId/tasks/:taskId', (req, res, next) => {
     let taskId: number = Number(req.params.taskId);
 
     db.findOne<Card>({id: cardId}, (err, doc) => {
-        if (err) {
-            next(err);
-        } else {
-            let currentTask = doc.tasks.find((task) => task.id === taskId);
-            currentTask.done = JSON.parse(req.body).done;
-            db.update({id: cardId}, doc, {}, (err, numReplaced) => {
-                if (err) {
-                    next(err);
-                } else {
-                    res.send(200);
-                    next();
-                }
-            });
-        }
+        next.ifError(err);
+        let currentTask = doc.tasks.find((task) => task.id === taskId);
+        currentTask.done = JSON.parse(req.body).done;
+        db.update({id: cardId}, doc, {}, (err, numReplaced) => {
+            next.ifError(err);
+            res.send(200, numReplaced);
+            next();
+        });
+
     });
 });
 
 server.put('/cards/:cardId', (req, res, next) => {
     let cardId: number = Number(req.params.cardId);
-    let card: CardModel = JSON.parse(req.body);
+    let card: Card = JSON.parse(req.body);
 
     db.findOne<Card>({id: cardId}, (err, doc) => {
-        if (err) {
-            next(err);
-        } else {
-            db.update({id: cardId}, card, {}, (err, numReplaced) => {
-                if (err) {
-                    next(err);
-                } else {
-                    res.send(200);
-                    next();
-                }
-            });
-        }
+        next.ifError(err);
+        db.update({id: cardId}, card, {}, (err) => {
+            next.ifError(err);
+            res.send(200, card);
+            next();
+        });
     })
 });
 
 server.post('/cards', (req, res, next) => {
-    let card: CardModel = JSON.parse(req.body);
+    let card: Card = JSON.parse(req.body);
 
     db.insert(card, (err, newDoc) => {
-        if (err) {
-            next(err);
-        } else {
-            res.send(newDoc);
-            res.send(200);
-            next();
-        }
+        next.ifError(err);
+        res.send(200, newDoc);
+        next();
     });
 });
 
